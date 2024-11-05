1f2bd01af760cd5628afb8ef56f091c4ae70948e
Bobi-Blockchain <bobi.blockchain@gmail.com>
2024-11-06 00:27:43 +0500

    New files merged

---
+++ frontend/build/static/js/216.4b3aa554.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkbobi_frontend=self.webpackChunkbobi_frontend||[]).push([[216],{84445:()=>{},50477:()=>{},51786:()=>{},59169:()=>{},86833:()=>{}}]);
--- frontend/src/components/Wallet.jsx
+++ frontend/src/components/Wallet.jsx
@@ -1,7 +1,44 @@
-export default function Wallet() {
+import { useEffect, useState } from "react";
+import { Contract, ethers } from "ethers";
+import abi from "../abi.json";
+
+export default function Wallet({ provider, open, isConnected, address }) {
+  const [text, setText] = useState("0 $WBoBi");
+
+  useEffect(() => {
+    async function execute() {
+      if (provider && isConnected && address) {
+        const signer = await provider.getSigner();
+
+        const contract = new Contract(
+          "0xA05edB1Def7cbf8f61B7C1aaE2786439796EB198",
+          abi,
+          signer
+        );
+
+        const balance = await contract.balanceOf(address);
+
+        const formattedEther = ethers.utils.formatEther(balance);
+        const float = parseFloat(formattedEther);
+        const formattedBalance = Math.round(float * 100000) / 100000;
+
+        setText(`${formattedBalance} $WBoBi`);
+      }
+    }
+
+    execute();
+  }, [provider, isConnected, address]);
+
   return (
-    <button className="connect-wallet-btn connect-wallet-state-btn">
-      Connect
+    <button
+      onClick={() => {
+        if (!isConnected) {
+          open();
+        }
+      }}
+      className="connect-wallet-btn connect-wallet-state-btn"
+    >
+      {isConnected ? text : "Connect"}
     </button>
   );
 }
